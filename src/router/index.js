5d4d638d4326bee31f5d7a5763bb48aecbd6f9f1
6onza <gvineg@fi.uba.ar>
2023-06-15 01:53:58 -0300

    Feat: Add products path and /:catchAll(.*) to redirect

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import AppHome from '../views/Home.vue'
 import AppAbout from '../views/About.vue'
 import AppContact from '../views/Contact.vue'
+import AppProducts from '../views/Products.vue'
 
 const routes = [
 
@@ -19,6 +20,16 @@ const routes = [
     path: '/contact',
     name: 'AppContact',
     component: AppContact
+  },
+  {
+    path: '/products',
+    name: 'AppProducts',
+    component: AppProducts
+  },
+  {
+    path: '/:catchAll(.*)',
+    redirect: '/'
+    // este es un catch all, si no encuentra la ruta, redirige a la home
   }
 ]
 
