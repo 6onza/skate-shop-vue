5702f39e77b871c87293d37cacf92215a77eb95e
6onza <gvineg@fi.uba.ar>
2023-06-26 05:35:41 -0300

    Feat: Added functionality for showing filters and minor CSS changes

--- src/views/Products.vue
+++ src/views/Products.vue
@@ -3,9 +3,10 @@
     :cartCount="cartCount"
     :productsOnCart="productsOnCart"
     @updateCart="updateCart"
+    :totalCartPrice="totalCartPrice"
   />
   <div
-    class="container-fluid p-0"
+    class="container-fluid p-0 mb-3"
     style="position: relative; margin-top: 170px; opacity: 0.9"
   >
     <img
@@ -14,81 +15,169 @@
       width="100%"
       style="
         filter: grayscale(100%);
-        height: 40vh;
+        height: 35vh;
+        width: 100%;
         object-fit: cover;
         object-position: 0 0;
         opacity: 1;
       "
     />
   </div>
-  <div
-    class="filtrar container pe-5 d-flex justify-content-end align-items-center mt-4"
-  >
-    <button
-      class="boton-filtros d-md-none d-block me-2 px-2"
-      type="button"
-      aria-expanded="false"
-    >
-      filtrar <i class="fa fa-angle-right"></i>
-    </button>
 
-    <div class="dropdown">
-      <button
-        class="dropdown-toggle boton-filtros px-2"
-        type="button"
-        id="dropdownMenuButton1"
-        data-bs-toggle="dropdown"
-        aria-expanded="false"
-      >
-        ordenar por
-      </button>
-      <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
-        <li><a class="dropdown-item" href="#">mayor precio</a></li>
-        <li><a class="dropdown-item" href="#">menor precio</a></li>
-        <li><a class="dropdown-item" href="#">mayor descuento</a></li>
-      </ul>
-    </div>
-  </div>
   <div class="container-fluid d-flex justify-content-center mb-5">
-    <div class="container-sm row d-flex justify-content-center px-0">
+    <div class="container-sm row d-flex justify-content-center px-2">
       <div
-        class="categorias flex-column d-flex col-12 col-md-2 d-none d-md-flex mt-5 pe-0 ps-md-3 ps-2"
-        id="categorias"
+        class="categorias flex-column d-flex col-12 col-md-2 d-flex pt-3 pt-md-5 pe-0 ps-md-3 ps-2 ps-4 pe-4"
+        :id="showFilters ? 'categorias' : 'hide-filters'"
       >
+        <div
+          class="btn close-filters-menu w-100 d-flex justify-content-end pe-2 pt-2 mb-3 d-md-none"
+        >
+          <button class="btn" @click="showFilters = false">
+            <i class="fas fa-times" style="font-size: 2rem"></i>
+          </button>
+        </div>
+        <div class="categoria pt-md-5 mt-md-5">
+          <h3>Marcas</h3>
+          <ul class="ps-md-1 ps-1 pt-2">
+            <input
+              type="checkbox"
+              id="vans"
+              name="vans"
+              value="vans"
+              v-model="selectedBrands"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="vans">vans</label
+            ><br />
+            <input
+              type="checkbox"
+              id="nike-sb"
+              name="nike-sb"
+              value="nike-sb"
+              v-model="selectedBrands"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="nike-sb">nike sb</label
+            ><br />
+            <input
+              type="checkbox"
+              id="adidas"
+              name="adidas"
+              value="adidas"
+              v-model="selectedBrands"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="adidas">adidas</label
+            >
+            <br />
+            <input
+              type="checkbox"
+              id="nike"
+              name="nike"
+              value="nike"
+              v-model="selectedBrands"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="nike">nike</label
+            ><br />
+            <input
+              type="checkbox"
+              id="dc"
+              name="dc"
+              value="dc"
+              v-model="selectedBrands"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="dc">dc</label
+            ><br />
+            <input
+              type="checkbox"
+              id="emerica"
+              name="emerica"
+              value="emerica"
+              v-model="selectedBrands"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="emerica">emerica</label
+            ><br />
+          </ul>
+        </div>
         <div class="categoria">
           <h3>skate</h3>
           <ul class="ps-md-1 ps-1 pt-2">
-            <input type="checkbox" id="tablas" name="tablas" value="tablas" />
+            <input
+              type="checkbox"
+              id="tablas"
+              name="tablas"
+              value="tablas"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
             <label class="ps-2" for="tablas">tablas</label
             ><br />
-            <input type="checkbox" id="ruedas" name="ruedas" value="ruedas" />
+            <input
+              type="checkbox"
+              id="ruedas"
+              name="ruedas"
+              value="ruedas"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
             <label class="ps-2" for="ruedas">ruedas</label
             ><br />
-            <input type="checkbox" id="ejes" name="ejes" value="ejes" />
-            <label class="ps-2" for="ejes">trucks</label
-            ><br />
-            <input type="checkbox" id="lija" name="lija" value="lija" />
-            <label class="ps-2" for="lija">lijas</label
+            <input
+              type="checkbox"
+              id="trucks"
+              name="trucks"
+              value="trucks"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="trucks">trucks</label
             ><br />
             <input
               type="checkbox"
               id="rulemanes"
               name="rulemanes"
               value="rulemanes"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="rulemanes">rulemanes</label
+            ><br />
+            <input
+              type="checkbox"
+              id="lija"
+              name="lija"
+              value="lija"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="lija">lijas</label
+            ><br />
+            <input
+              type="checkbox"
+              id="tornillos"
+              name="tornillos"
+              value="tornillos"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="tornillos">tornillos</label
             ><br />
             <input
               type="checkbox"
               id="skate-completo"
               name="skate-completo"
               value="skate-completo"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
-            <label class="ps-2" for="skate-completo">skate completos</label
+            <label class="ps-2" for="skate-completo">skate completo</label
             ><br />
           </ul>
         </div>
-        <hr class="me-5" />
         <div class="categoria">
           <h3>indumentaria</h3>
           <ul class="ps-md-1 ps-1 pt-2">
@@ -97,10 +186,19 @@
               id="remeras"
               name="remeras"
               value="remeras"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="remeras">remeras</label
             ><br />
-            <input type="checkbox" id="buzos" name="buzos" value="buzos" />
+            <input
+              type="checkbox"
+              id="buzos"
+              name="buzos"
+              value="buzos"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
             <label class="ps-2" for="buzos">buzos</label
             ><br />
             <input
@@ -108,6 +206,8 @@
               id="camperas"
               name="camperas"
               value="camperas"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="camperas">camperas</label
             ><br />
@@ -116,6 +216,8 @@
               id="pantalones"
               name="pantalones"
               value="pantalones"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="pantalones">pantalones</label
             ><br />
@@ -124,10 +226,19 @@
               id="zapatillas"
               name="zapatillas"
               value="zapatillas"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="zapatillas">zapatillas</label
             ><br />
-            <input type="checkbox" id="gorras" name="gorras" value="gorras" />
+            <input
+              type="checkbox"
+              id="gorras"
+              name="gorras"
+              value="gorras"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
             <label class="ps-2" for="gorras">gorras</label
             ><br />
             <input
@@ -135,51 +246,68 @@
               id="mochilas"
               name="mochilas"
               value="mochilas"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="mochilas">mochilas</label
             ><br />
-            <input
-              type="checkbox"
-              id="calcetines"
-              name="calcetines"
-              value="calcetines"
-            />
-            <label class="ps-2" for="calcetines">medias</label
-            ><br />
           </ul>
         </div>
-        <hr class="me-5" />
         <div class="categoria">
           <h3>accesorios</h3>
           <ul class="ps-md-1 ps-1 pt-2">
             <input
               type="checkbox"
+              id="skate-tool"
+              name="skate-tool"
+              value="skate-tool"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
+            <label class="ps-2" for="skate-tool">skate tool</label
+            ><br />
+            <input
+              type="checkbox"
               id="llaveros"
               name="llaveros"
               value="llaveros"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
             <label class="ps-2" for="llaveros">llaveros</label
             ><br />
             <input
               type="checkbox"
-              id="billeteras"
-              name="billeteras"
-              value="billeteras"
+              id="cera"
+              name="cera"
+              value="cera"
+              v-model="selectedCategories"
+              @change="filterProducts"
             />
-            <label class="ps-2" for="billeteras">billeteras</label
+            <label class="ps-2" for="cera">cera</label
             ><br />
-            <input type="checkbox" id="llaves" name="llaves" value="llaves" />
+            <input
+              type="checkbox"
+              id="llaves"
+              name="llaves"
+              value="llaves"
+              v-model="selectedCategories"
+              @change="filterProducts"
+            />
             <label class="ps-2" for="llaves">llaves</label
             ><br />
           </ul>
         </div>
         <!-- aplicar filtros -->
-        <div class="d-flex my-2 row flex-column pe-5" style="font-size: 0.8rem">
+        <div
+          class="d-flex my-2 row flex-column pe-4 ms-md-0 ms-1 pb-2 pe-md-3"
+          style="font-size: 0.8rem"
+        >
           <button
             type="button"
             class="basic-button-white my-2"
             @click="applyFilters"
-            style="font-size: 1rem;"
+            style="font-size: 1rem"
           >
             <i class="fas fa-filter"></i>
             aplicar filtros
@@ -188,16 +316,59 @@
           <button
             v-if="filtersApplied"
             type="button"
-            class="basic-button-white"
+            class="basic-button-white my-2"
             @click="clearFilters"
-            style="font-size: 1rem;"
+            style="font-size: 1rem"
           >
             <i class="fas fa-trash-alt"></i>
             eliminar filtros
           </button>
         </div>
       </div>
-      <div class="col-12 col-md-10 row p-0">
+      <div class="col-12 col-md-10 row p-0 d-flex">
+        <div
+          class="col-6 d-flex justify-content-end align-items-center pt-3 pe-2"
+        >
+          <button
+            v-if="!showFilters"
+            class="boton-filtros d-md-none d-block px-3 py-2"
+            @click="showFilters = !showFilters"
+          >
+            <i class="fas fa-filter"></i>
+            filtrar
+          </button>
+        </div>
+        <div
+          class="dropdown text-end pt-3 col-6 d-flex justify-content-md-end justify-content-start ms-0 ps-0"
+        >
+          <button
+            class="dropdown-toggle boton-filtros px-2 py-2"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+            aria-expanded="false"
+          >
+            ordenar por
+          </button>
+          <ul class="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+            <li>
+              <button
+                class="dropdown-item"
+                @click="showProductsSortedByPrice('desc')"
+              >
+                mayor precio
+              </button>
+            </li>
+            <li>
+              <button
+                class="dropdown-item"
+                @click="showProductsSortedByPrice('asc')"
+              >
+                menor precio
+              </button>
+            </li>
+          </ul>
+        </div>
         <AppProduct
           v-for="product in products"
           :key="product.id"
@@ -214,14 +385,18 @@
           <button
             v-if="!loading && hasMore"
             type="button"
-            @click="showMoreProducts"
+            @click="showMoreProducts(showMoreProductsFilter)"
             class="basic-button-white"
-            style="font-size: 1rem;"
+            style="font-size: 1rem"
           >
             mostrar mas
             <i class="fas fa-chevron-down"></i>
           </button>
-          <div v-if="loading" class="spinner-loading text-primary" role="status">
+          <div
+            v-if="loading"
+            class="spinner-loading text-primary"
+            role="status"
+          >
             <span class="visually-hidden">Loading...</span>
           </div>
         </div>
@@ -253,8 +428,11 @@ export default {
       products: [],
       selectedCategories: [],
       filtersApplied: false,
-      sortBy: "name",
+      sortBy: "all",
       loading: false,
+      showMoreProductsFilter: "all",
+      showFilters: true,
+      totalCartPrice: 0,
     };
   },
   mounted() {
@@ -273,9 +451,17 @@ export default {
       this.loading = true;
       this.fetchProducts();
     },
-    showMoreProducts() {
+    showMoreProducts(filter) {
       this.loading = true;
-      this.fetchMoreProducts();
+      if (this.filtersApplied) {
+        this.fetchMoreProductsByCategory(filter);
+      }
+      if (this.sortBy === "all") {
+        this.fetchMoreProducts(filter);
+      }
+      if (this.sortBy != "all") {
+        this.fetchMoreProductsSortedByPrice(this.sortBy);
+      }
     },
     applyFilters() {
       // Recopilar las categorías seleccionadas
@@ -287,25 +473,38 @@ export default {
       this.products = [];
       this.filtersApplied = true;
       this.nextPage = 1;
+      this.showMoreProductsFilter = "all";
 
       this.selectedCategories.forEach((category) => {
         this.fetchCategoryProducts(category);
       });
+
+      if (this.selectedCategories.length === 0) {
+        this.filtersApplied = false;
+        this.loadProducts();
+      }
     },
     clearFilters() {
       this.products = [];
       this.selectedCategories = [];
       this.filtersApplied = false;
       this.nextPage = 1;
+      this.sortBy = "all";
       this.loadProducts();
     },
-    
+    showProductsSortedByPrice(order) {
+      this.products = [];
+      this.nextPage = 1;
+      this.sortBy = order;
+      this.fetchProductsSortedByPrice(order);
+    },
   },
 };
 </script>
 
 <style scoped>
 @import url("https://fonts.googleapis.com/css2?family=Roboto+Condensed&display=swap");
+@import url("https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap");
 
 .spinner-loading {
   width: 2rem;
@@ -317,17 +516,7 @@ export default {
   border-radius: 50%;
   animation: spinner-border 0.75s linear infinite;
 }
-.boton-filtros {
-  background-color: #ffffff;
-  color: #000000;
-  border: none;
-  border-radius: 0;
-  font-size: 1rem;
-  font-weight: 500;
-  padding: 0.5rem;
-  transition: all 0.3s ease-in-out;
-  border: 1px solid #000;
-}
+
 .categorias-mobile {
   width: 100%;
   margin: 0 auto;
@@ -340,10 +529,20 @@ export default {
   display: block !important;
 }
 .no-hay-productos {
-  font-family: "Roboto Condensed", sans-serif;
+  font-family: "Bebas Neue", cursive;
   font-size: 1.5rem;
-  font-weight: 600;
   color: #000000;
+}
+.boton-filtros {
+  font-family: "Bebas Neue", cursive;
+  font-size: 1rem;
+  letter-spacing: 0.1rem;
+  background-color: #ffffff;
+  color: #000000;
+  border: 1px solid #000;
+  transition: all 0.3s ease-in-out;
+  height: 3rem;
+  border-radius: 0.5rem;
 }
 .boton-filtros:hover {
   background-color: #000;
@@ -369,6 +568,8 @@ export default {
   font-weight: 500;
   padding: 0.5rem;
   transition: all 0.3s ease-in-out;
+  font-family: "Bebas Neue", cursive;
+  letter-spacing: 0.1rem;
 }
 .dropdown-item:hover {
   background-color: #000;
@@ -380,20 +581,19 @@ export default {
   font-size: 1.2rem;
   font-weight: 500;
   margin: 0 0.5rem;
-  transition: all 0.3s ease-in-out;
   border: 1px solid #000;
+  border-radius: 0.5rem;
 }
 .categoria {
   width: 100%;
   margin: 0 auto;
-  padding: 0;
 }
 .categoria h3 {
-  font-size: 1.3rem;
+  font-size: 1.5rem;
   font-weight: 500;
   margin: 0;
   padding: 0;
-  font-family: "Roboto Condensed", sans-serif;
+  font-family: "Bebas Neue", cursive;
 }
 input[type="checkbox"] {
   width: 15px;
@@ -405,20 +605,62 @@ input[type="checkbox"] {
 }
 label {
   font-family: "Roboto Condensed", sans-serif;
-  font-size: 1rem;
+  font-size: 0.9rem;
   color: #424242;
 }
 .basic-button-white {
-  background-color: #ffffff;
-  color: #000000;
-  border: 1px solid #000000;
+  background-color: #000000;
+  color: #ffffff;
   padding: 10px 15px 10px 15px;
   cursor: pointer;
+  border-radius: 0.5rem;
   height: 50px;
 }
 .basic-button-white:hover {
-  background-color: #000000;
-  color: #ffffff;
+  background-color: #afafaf;
+  color: #0e0e0e;
   transition: 0.2s;
+}
+#hide-filters {
+  display: none !important;
+}
+@media (max-width: 768px) {
+  #categorias {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100vh;
+    background-color: white;
+    z-index: 9999;
+    animation: slide-in 0.5s backwards;
+  }
+
+  
+  @keyframes slide-in {
+    0% {
+      transform: translateX(100vw);
+    }
+    100% {
+      transform: translateX(0);
+    }
+  }
+  .categoria h3 {
+    font-size: 1.5rem;
+  }
+  input[type="checkbox"] {
+    width: 20px;
+    height: 20px;
+    margin: 0;
+    padding: 0;
+    border: 1px solid #000;
+    border-radius: 0;
+  }
+  label {
+    font-family: "Roboto Condensed", sans-serif;
+    font-size: 1.2rem;
+    color: #424242;
+  }
+
 }
 </style>
