b42512fa20edd366251d9c71d4cb677d92a737d8
6onza <gvineg@fi.uba.ar>
2023-06-26 05:34:05 -0300

    Feat: Added functionality to find relatedProducts and for find total cart price

--- src/mixin.js
+++ src/mixin.js
@@ -1,20 +1,44 @@
 import axios from "axios";
 
 export const cartMixin = {
+  data() {
+    return {
+      productsOnCart: [],
+      totalCartPrice: 0,
+      cartCount: 0,
+    };
+  },
   methods: {
     getProductsOnCart() {
       const productsOnCart = JSON.parse(localStorage.getItem("cart"));
       this.productsOnCart = productsOnCart || [];
     },
+    calculateTotal() {
+      this.totalCartPrice = 0;
+      this.productsOnCart.forEach((product) => {
+        this.totalCartPrice += parseInt(product.price);
+      }
+      );
+    },
     updateCart() {
       const cartCount = JSON.parse(localStorage.getItem("cart"))?.length;
       this.cartCount = cartCount || 0;
       this.getProductsOnCart();
+      this.calculateTotal();
     },
   },
   mounted() {
     this.updateCart();
+
   },
+  watch: {
+    productsOnCart() {
+      localStorage.setItem("cart", JSON.stringify(this.productsOnCart));
+      this.calculateTotal();
+    }
+  }
+
+  
 };
 
 export const productMixin = {
@@ -23,6 +47,7 @@ export const productMixin = {
       products: [],
       nextPage: 1,
       hasMore: true,
+      relatedProducts: [],
     };
   },
   methods: {
@@ -54,6 +79,14 @@ export const productMixin = {
         .get(`https://skate-shop-api.vercel.app/api/products/all/${id}/`)
         .then((response) => {
           this.product = response.data
+          axios
+            .get(`https://skate-shop-api.vercel.app/api/products/category/${this.product.category}/`)
+            .then((response) => {
+              // el filter es para que no aparezca el producto que estamos viendo en la lista de productos relacionados
+              this.relatedProducts = response.data.results.filter(product => product.id !== this.product.id)
+            }
+            )
+          
         })
         .catch((error) => {
           console.log(error);
@@ -70,6 +103,38 @@ export const productMixin = {
         .catch((error) => {
           console.log(error);
         });
+    },
+    fetchMoreProductsByCategory(category) {
+      if (this.hasMore) {
+        this.fetchCategoryProducts(category);
+      }
+    },
+
+    fetchProductsSortedByPrice(order) {
+      axios
+        .get("https://skate-shop-api.vercel.app/api/products/ordered/" + order + "/", {
+          params: {
+            page: this.nextPage,
+          }
+        })
+        .then((response) => {
+          this.loading = false;
+          const products = response.data.results;
+          this.products = [...this.products, ...products];
+          this.hasMore = response.data.next ? true : false;
+          this.nextPage++;
+        }
+        )
+        .catch((error) => {
+          console.log(error);
+        }
+      );
+
+    },
+    fetchMoreProductsSortedByPrice(order) {
+      if (this.hasMore) {
+        this.fetchProductsSortedByPrice(order);
+      }
     }
   },
 };
